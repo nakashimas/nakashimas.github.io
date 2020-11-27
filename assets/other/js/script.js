8389b51a5f1a930a71589e719388b28765f409e8
nakasimas <nascor.neco@gmail.com>
2020-11-27 18:05:40 +0900

    add TOC

--- assets/other/js/script.js
+++ assets/other/js/script.js
@@ -11,20 +11,20 @@ $(document).ready(function() {
 
 	// Smooth scroll
 	$('a.scroll-to').on('click', function (event) {
-	var $anchor = $(this);
-	$('html, body').stop().animate({
-		scrollTop: ($($anchor.attr('href')).offset().top - 50)
-	}, 700);
-	event.preventDefault();
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: ($($anchor.attr('href')).offset().top - 50)
+		}, 700);
+		event.preventDefault();
 	});
 
 	$('.site-testimonial-item').on('mouseenter', function(){
-	$('.site-testimonial-item').addClass('inactive');
-	$(this).removeClass('inactive').addClass('active');
-	});
-	$('.site-testimonial-item').on('mouseleave', function(){
-	$('.site-testimonial-item').removeClass('inactive');
-	$('.site-testimonial-item').removeClass('active');
+		$('.site-testimonial-item').addClass('inactive');
+		$(this).removeClass('inactive').addClass('active');
+		});
+		$('.site-testimonial-item').on('mouseleave', function(){
+		$('.site-testimonial-item').removeClass('inactive');
+		$('.site-testimonial-item').removeClass('active');
 	});
 });
 
